be8a298011a1524d01afa3b05447b2050b7896e2
miladezzat <miladezzat.f@gmail.com>
2020-08-27 21:02:29 +0200

    testing

--- front/src/pages/Home/home.js
+++ front/src/pages/Home/home.js
@@ -3,6 +3,8 @@ import milad from '../../assets/milad.jpeg';
 function Home() {
     return <div style={{textAlign: 'center', paddingTop: '3%'}}>
         <img style={{width: '100px', borderRadius: '50%', boxShadow: '0 0 5px black'}} src={milad} alt='milad' />
+        <h2 style={{padding:0, margin:0}}>Milad E. Fahmy</h2>    
+        <p style={{padding:0, margin:0, opacity:0.4}}>Software Engineer</p>
     </div>
 }
 
